252f2a0e5c3b4dd4a766b148d530c02b623a435d
Yuntong Xie <xieyuntong2000@gmail.com>
2023-05-05 18:29:04 -0500

    init

---
+++ serve.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def process_json():
+    if request.method == 'POST':
+        p = subprocess.Popen(['python3', 'stress_cpu.py'])
+        print("stress_cpu run ")
+        return None
+    elif request.method == 'GET':
+        return socket.gethostname()
+    else:
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
